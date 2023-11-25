4cec27c31e5436b65e2b11539d7b4de4f3a505ee
Bart1251 <bartlomiej.wiecek@vp.pl>
2023-11-25 16:51:58 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Home/>}/>
+            <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Page not found 404</h1>
+        </div>
+    )
+}
